c620887fe4d472820a385e57a5302b292aa8f1f8
Man <qmanqm97@gmail.com>
2025-04-05 10:14:45 +0700

    add index.php file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    
+    // Scalar types (contains one value)
+    $string = "Daniel";
+    $int = 123456;
+    $float = 2.5678;
+    $bool = true;
+    
+    //Array type
+    $name = ["Daniel", "Peter", "Quang"];
+
+
+?>
